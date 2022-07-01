e040987feb45a2a0962e7f454c9b2a8330b774ad
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-07-01 10:05:39 -0300

    Lift-off lab

--- src/schema.js
+++ src/schema.js
@@ -6,6 +6,7 @@ const typeDefs = gql`
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
     track(id: ID!): Track
+    module(id: ID!): Module!
   }
 
   type Mutation {
@@ -53,6 +54,10 @@ const typeDefs = gql`
     length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's video duration, in seconds"
     durationInSeconds: Int
+    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    content: String
+    "The module's video url, for video-based modules"
+    videoUrl: String
   }
 
   type IncrementTrackViewsResponse {
